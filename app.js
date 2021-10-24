efb47a11504ba7707db996f551fb525e74fa8464
JuliaDonadelli <89952741+JuliaDonadelli@users.noreply.github.com>
2021-10-23 22:10:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+function MostrarCampoPesquisar(){
+    let campo = document.querySelector("#campo")
+    if(campo.style.display = "inline"){
+        campo.style.display = "none"    
+    }else{
+        campo.style.display = "inline"
+    }
+}
+
+/*
+projetos = {
+    titulo: 'Karen',
+    descricao: 'Projeto desenvolvido para identidade vivual' , 
+    foto: 'kscapalogo.jpg'
+
+}
+
+okashi = {
+    titulo: 'Jdmarca',
+    descricao: 'Projeto da minha logo',  
+    foto: 'okashitye'
+
+}
+
+jdlogo = {
+    titulo: 'Jdmarca',
+    descricao: 'Projeto da minha logo', 
+    foto: 'jda.png'
+}
+*/
+
+//let projetos = ['Identidade Visual Karen Santos' ,'Social Media para Okashi Doceria', 'JD minha Marca']
+//let projetosDescricao = [
+  //  'Projeto desenvolvido para utilização web com fins de divulgamento do trabalho da Nail Designer Karen Santos.',
+    //'Projeto de Social Media desenvolvido para Doceria, afim de divulgar seus doces no instagram',
+    //'Projeto da minha logo, onde exploro meu cliente mais exigente, ITS ME !.'
+//]
+
+let fotos = ['kscapalogo.jpg' , 'okashitye.jpg' , 'logoeu.png']
+
+let projetoAtual = 0 
+
+function ProximoProjeto(){
+    if(projetoAtual < 2){
+        projetoAtual++
+    }else{
+    projetoAtual = 0
+}
+
+     titulo = document.querySelector(".titulo-projeto")
+     titulo.innerHTML = projetos[projetoAtual]
+    
+     descricao = document.querySelector(".texto-corpo2")
+     descricao.innerHTML = projetosDescricao[projetoAtual]
+
+}
